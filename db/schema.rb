55ec0bcc8c62b74de0e2db7e5e0332d29bd72298
stevenmathews <stevenmathews.nz@gmail.com>
2014-09-25 18:12:14 +1200

    game reference added to player table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140925054438) do
+ActiveRecord::Schema.define(version: 20140925060831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(version: 20140925054438) do
     t.integer  "lng"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "game_id"
   end
+
+  add_index "players", ["game_id"], name: "index_players_on_game_id", using: :btree
 
 end
