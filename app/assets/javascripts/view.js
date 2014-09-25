c5f90e2488ef17e6a04f9e6494c39c83ded69907
Amelia Laundy <amelialaundy@gmail.com>
2014-09-25 22:27:56 +1200

    set zoom to optimal, disbaled dragging, dbl click zoom, pegman, and zoom control

--- app/assets/javascripts/view.js
+++ app/assets/javascripts/view.js
@@ -1,7 +1,7 @@
 function View() {
 	this.lat = -41.295308
 	this.lng = 174.773082
-	this.zoom = 8
+	this.zoom = 15
 
 }
 
@@ -10,7 +10,12 @@ View.prototype = {
 		console.log("hey")
       var mapOptions = {
         center: new google.maps.LatLng(this.lat, this.lng),
-        zoom: this.zoom
+        zoom: this.zoom,
+        draggable: false,
+        zoomControl: false,
+        scrollwheel: false,
+        disableDoubleClickZoom: true,
+        streetViewControl: false
       }
       this.map = new google.maps.Map(document.getElementById("map-canvas"),
         mapOptions);
