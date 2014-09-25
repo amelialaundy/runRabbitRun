99cae3a699ca2664650295ab868af9ff4300294b
stevenmathews <stevenmathews.nz@gmail.com>
2014-09-25 18:13:59 +1200

    updated column name in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 
-Game.create(centre_lat: -41.295291, centre_long: 174.773071, num_players: 2)
+Game.create(centre_lat: -41.295291, centre_lng: 174.773071, num_players: 2)
 Player.create(kind: "rabbit", lat: -41.295153, lng: 174.772898, game_id: 1)
 Player.create(kind: "hunter", lat: -41.295260, lng: 174.772480, game_id: 1)
