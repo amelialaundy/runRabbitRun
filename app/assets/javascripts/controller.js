6eddae97a05397afc2a72e7108c9c415746e01fd
Amelia Laundy <amelialaundy@gmail.com>
2014-09-25 23:30:01 +1200

    all four directions created new markers in that direction according to which arrow key is pressed

--- app/assets/javascripts/controller.js
+++ app/assets/javascripts/controller.js
@@ -31,12 +31,28 @@ Controller.prototype = {
 		// 40 = down
 
 		if (e.keyCode == 38) {
-			this.view.googlePlayer.position.k = this.playerOptions.lat + 0.002
-			this.view.googlePlayer.position.B = 174.773259
+			this.view.googlePlayer.position.k = this.playerOptions.lat + 0.00008
+			this.view.googlePlayer.position.B = this.playerOptions.lng
 			this.playerOptions.lat = this.view.googlePlayer.position.k
 			this.playerOptions.lng = this.view.googlePlayer.position.B
-			console.log(this.view.googlePlayer.position.k)
-			console.log(this.view.googlePlayer.position.B)
+			this.createPlayerMarkers(this.playerOptions)
+		} else if (e.keyCode == 39) {
+			this.view.googlePlayer.position.k = this.playerOptions.lat
+			this.view.googlePlayer.position.B = this.playerOptions.lng + 0.00008
+			this.playerOptions.lat = this.view.googlePlayer.position.k
+			this.playerOptions.lng = this.view.googlePlayer.position.B
+			this.createPlayerMarkers(this.playerOptions)
+		} else if (e.keyCode == 40) {
+			this.view.googlePlayer.position.k = this.playerOptions.lat - 0.00008
+			this.view.googlePlayer.position.B = this.playerOptions.lng 
+			this.playerOptions.lat = this.view.googlePlayer.position.k
+			this.playerOptions.lng = this.view.googlePlayer.position.B
+			this.createPlayerMarkers(this.playerOptions)
+		} else if (e.keyCode == 37) {
+			this.view.googlePlayer.position.k = this.playerOptions.lat
+			this.view.googlePlayer.position.B = this.playerOptions.lng - 0.00008
+			this.playerOptions.lat = this.view.googlePlayer.position.k
+			this.playerOptions.lng = this.view.googlePlayer.position.B
 			this.createPlayerMarkers(this.playerOptions)
 		}
 	}
