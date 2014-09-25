7f0b8c712d2cf4cdd429853c77f250dc0b37c512
Amelia Laundy <amelialaundy@gmail.com>
2014-09-25 22:19:59 +1200

    hard coding starting lat and lng of map and adding relevant scripts to application html view

--- app/assets/javascripts/view.js
+++ app/assets/javascripts/view.js
@@ -1,9 +1,13 @@
 function View() {
+	this.lat = -41.295308
+	this.lng = 174.773082
+	this.zoom = 8
 
 }
 
 View.prototype = {
 	initializeMap: function() {
+		console.log("hey")
       var mapOptions = {
         center: new google.maps.LatLng(this.lat, this.lng),
         zoom: this.zoom
