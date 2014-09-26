edee5aa88d9d409c8d28bb3ede1e9763dc049ba8
Amelia Laundy <amelialaundy@gmail.com>
2014-09-26 21:43:47 +1200

    increasing area of random ats and longs

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,11 +3,11 @@ class Game < ActiveRecord::Base
 
   def get_random_lat
     puts "this is the centre lat:#{centre_lat}"
-    centre_lat + ((rand(1000..2000))/1000000.00)
+    centre_lat + ((rand(3000..8000))/1000000.00)
   end
 
   def get_random_lng
-    centre_lng + ((rand(1000..2000))/1000000.00)
+    centre_lng + ((rand(3000..8000))/1000000.00)
   end
 
   def mark_as_rabbit?
