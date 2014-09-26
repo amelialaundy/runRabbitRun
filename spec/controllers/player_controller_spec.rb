7901f1b6e64d5081cce38db16ce4a89e88d8327b
stevenmathews <stevenmathews.nz@gmail.com>
2014-09-26 15:14:07 +1200

    player_controller tests updated and passed

--- spec/controllers/player_controller_spec.rb
+++ spec/controllers/player_controller_spec.rb
@@ -2,11 +2,19 @@ require 'rails_helper'
 
 RSpec.describe PlayerController, :type => :controller do
 
-	describe 'database updated' do
+	describe 'game_status update' do
 
-		it "" do
-      post :update_player_position, {id: 1, lat: -41.295260, lng: 174.772480, game_id: 1}
+		let(:params) {{"id"=>"1", "lat"=>"-41.29526", "lng"=>"174.77248", "game_id"=>"1", "controller"=>"player", "action"=>"update_player_position"}}
 
+		it "#update receives json from ajax" do
+			expect(GameStatus).to receive(:update).with(params)
+      post :update_player_position, params
+		end
+
+		it "returns the game status" do
+			allow(GameStatus).to receive(:update).and_return(true)
+			post :update_player_position, params
+			expect(response.body).to eq("{\"game_status\":true}")
 		end
   
 	end
