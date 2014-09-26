a7fd8d18748c4085f9106abe6b1326e8e56198c4
Amelia Laundy <amelialaundy@gmail.com>
2014-09-26 12:58:01 +1200

    removing development log and tmp cache sprocket files

--- app/assets/javascripts/controller.js
+++ app/assets/javascripts/controller.js
@@ -7,7 +7,9 @@ function Controller() {
       game_id: 1
     };
     this.player = null;
+    this.timer = null;
     this.updatePlayerUrl = '/player/update_position'
+    var self = this
 }
 
 Controller.prototype = {
@@ -15,16 +17,16 @@ Controller.prototype = {
 		this.bindEvents();
 		this.view.initializeMap();
 		this.createPlayerMarkers(this.playerOptions);
-		this.setUpTimer();
+		this.setUpTimer(1000);
 	},
 
 	bindEvents: function() {
 		document.addEventListener("keyup", this.movePlayerMarker.bind(this), false);
 	},
 
-	setUpTimer: function() {
+	setUpTimer: function(interval) {
 		var self = this
-		setInterval(this.sendPlayerPosition.bind(this), 1000)
+		self.timer = setInterval(this.sendPlayerPosition.bind(this), interval)
 	},
 
 	sendPlayerPosition: function() {
@@ -32,12 +34,14 @@ Controller.prototype = {
 		  type: "POST",
 		  url: this.updatePlayerUrl,
 		  data: this.playerOptions,
-		  success: this.checkWinState
+		  success: this.checkWinState.bind(this)
 		});
 	},
 
 	checkWinState: function(data) {
 		console.log(data)
+		console.log(this)
+		clearInterval(this.timer)
 	},
 
 	createPlayerMarkers: function(player) {
