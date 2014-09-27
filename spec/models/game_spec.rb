2593f8c123dc6841110721e84e48d0f0584d8609
Adam Ron <apprentice@Fiji.local>
2014-09-27 17:17:43 +1200

    game unit tests written

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Game, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:game) { create(:game) }
+
+  describe "get_random_lat" do
+    it "returns a random latitude within 0.005 of centre" do
+      expect(game.get_random_lat).to be_within(0.005).of(41.295260)
+    end
+  end
+  describe "get_random_lng" do
+    it "returns a random latitude within 0.005 of centre" do
+      expect(game.get_random_lng).to be_within(0.005).of(41.295260)
+    end
+  end
+
 end
