94d5289b5b460bccd09cccbf7864317389ac0b65
Adam Ron <apprentice@Fiji.local>
2014-09-28 12:53:59 +1300

    added mark_as_rabbit test

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -9,10 +9,18 @@ RSpec.describe Game, :type => :model do
       expect(game.get_random_lat).to be_within(0.005).of(41.295260)
     end
   end
+
   describe "get_random_lng" do
     it "returns a random latitude within 0.005 of centre" do
       expect(game.get_random_lng).to be_within(0.005).of(174.772480)
     end
+  end
+
+  describe "mark_as_rabbit?" do
+    it "gives the player who starts the game the 'rabbit' kind" do
+      expect(game.mark_as_rabbit?).to eq('rabbit')
+    end
+
   end
 
 end
