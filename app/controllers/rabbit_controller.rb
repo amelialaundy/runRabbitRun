eea8d1a4767321fb5bf07637abde2a636d98ded7
Amelia Laundy <amelialaundy@users.noreply.github.com>
2014-09-29 09:48:13 +1300

    removing game_data = params
     no need for duplicate variabeles

--- app/controllers/rabbit_controller.rb
+++ app/controllers/rabbit_controller.rb
@@ -1,12 +1,12 @@
 class RabbitController < ApplicationController
 
   def update_rabbit_street_view
-    game_data = params
-    latlng = game_data["lat"]+','+game_data["lng"]
+    
+    latlng = params["lat"]+','+params["lng"]
 
-    Pusher['rabbit_location_game_'+game_data["game_id"].to_s].trigger('show_rabbit_street_view_game', {
+    Pusher['rabbit_location_game_'+params["game_id"].to_s].trigger('show_rabbit_street_view_game', {
       :message => latlng
     })
-    render json: {game_status: game_data}
+    render json: {game_status: params}
   end
 end
