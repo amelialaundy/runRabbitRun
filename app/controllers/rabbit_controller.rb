4e02e90b2779e22a79ca0459dbae24fade5da63d
Adam Ron <apprentice@Fiji.local>
2014-09-29 11:41:43 +1300

    looking at Pusher-fake documentation

--- app/controllers/rabbit_controller.rb
+++ app/controllers/rabbit_controller.rb
@@ -1,12 +1,10 @@
 class RabbitController < ApplicationController
 
   def update_rabbit_street_view
-    
     latlng = params["lat"]+','+params["lng"]
-
     Pusher['rabbit_location_game_'+params["game_id"].to_s].trigger('show_rabbit_street_view_game', {
       :message => latlng
     })
-    render json: {game_status: params}
+    render json: {latlng: latlng}
   end
 end
