a7d1764b158caea2cdc607582a484dd9bea4cb7f
Amelia Laundy <amelialaundy@gmail.com>
2014-09-28 18:23:16 +1300

    tests passing for creating player marker function of controller

--- spec/javascripts/controller_spec.js
+++ spec/javascripts/controller_spec.js
@@ -1,10 +1,10 @@
 describe("PlayerController", function() {
 	var controller = null;
+
 	describe("attributes on creation", function() {
-		var myBindEvents = null;
 	  beforeEach(function() {
 	  	var myView = spyOn(window, 'View');
-	  	var myBindEvents = spyOn(document, 'addEventListener');
+	  	
 	  	controller = new PlayerController();
 	  });
 
@@ -36,18 +36,27 @@ describe("PlayerController", function() {
 	    expect(controller.start).toBeDefined();
 	  });
 
-	  it("bind events adds a keyup event listener to the document", function() {
-	  	controller.bindEvents();
-	  	this.bindEventsArgs = document.addEventListener.calls.argsFor(0);
-	    expect(this.bindEventsArgs[0]).toEqual('keyup');
-	  });
-
-	  it("bind events sets the keyup call back to the sendPlayerPosition function", function() {
-	  	controller.bindEvents();
-	  	this.bindEventsArgs = document.addEventListener.calls.argsFor(0);
-	    expect(this.bindEventsArgs[1]).toEqual(jasmine.any(Function));
-	  });
+	  
 	})
+
+describe("bind events function", function() {
+
+	beforeEach(function() {
+		var myBindEvents = spyOn(document, 'addEventListener');
+	})
+
+	it("bind events adds a keyup event listener to the document", function() {
+		controller.bindEvents();
+		this.bindEventsArgs = document.addEventListener.calls.argsFor(0);
+	  expect(this.bindEventsArgs[0]).toEqual('keyup');
+	});
+
+	it("bind events sets the keyup call back to the sendPlayerPosition function", function() {
+		controller.bindEvents();
+		this.bindEventsArgs = document.addEventListener.calls.argsFor(0);
+	  expect(this.bindEventsArgs[1]).toEqual(jasmine.any(Function));
+	});
+})
 
 	describe("sending player position", function() {
 		beforeEach(function() {
@@ -70,29 +79,76 @@ describe("PlayerController", function() {
 	    expect(this.requestArgs[0].type).toEqual('POST');
 	  });
 
-  	it("makes a POST request to '/player/update_position' ", function () {
+  	it("sends the request to '/player/update_position' ", function () {
       expect(this.requestArgs[0].data).toEqual(controller.playerOptions);
     });
 
 	})
 
-		describe("check win state", function() {
-			beforeEach(function() {
-				spyOn(window, 'clearInterval');
-				spyOn(window, 'alert')
-
-				// spyOn(PlayerController, 'locationTimer');
-				this.data = {
-				    game_status: true
-				};
-				controller.checkWinState(this.data);
-			})
-
-			it("stops the timer when the game is won", function () {
-		    expect(clearInterval).toHaveBeenCalled();
-		  });
-
+	describe("check win state", function() {
+		beforeEach(function() {
+			spyOn(window, 'clearInterval');
+			spyOn(window, 'alert')
+			
 		})
+
+		it("stops the timer when the game is won", function () {
+			this.data = {
+			    game_status: true
+			};
+			controller.checkWinState(this.data);
+	    expect(clearInterval).toHaveBeenCalledWith(controller.locationTimer);
+	  });
+
+  	it("doesnt stops the timer when the game is won", function () {
+  		this.data = {
+  		    game_status: false
+  		};
+  		controller.checkWinState(this.data);
+      expect(clearInterval).not.toHaveBeenCalled();
+    });
+
+	})
+
+	describe("create player markers", function() {
+
+		beforeEach(function() {
+			spyOn(window, 'PlayerMarker');
+			controller.view = {
+				playerIdDiv: 1,
+				playerLatDiv: 2,
+				playerLngDiv: 3,
+				gameIdDiv: 4,
+				playerKindDiv: "rabbit",
+
+				renderMapPlayerMarkers: function() {}
+			}
+			spyOn(controller.view, "renderMapPlayerMarkers")
+			controller.createPlayerMarkers();
+		})
+
+		it("sets the player options id from info from the view", function () {
+			
+	    expect(controller.playerOptions.id).toBe(1);
+	  });
+
+	  it("sets the player options lat from info from the view", function () {
+	    expect(controller.playerOptions.lat).toBe(2);
+	  });
+
+	  it("sets the player options lng from info from the view", function () {
+	    expect(controller.playerOptions.lng).toBe(3);
+	  });
+
+	  it("sets the player options lng from info from the view", function () {
+	    expect(controller.playerOptions.game_id).toBe(4);
+	  });
+
+	  it("creates a new player marker object", function () {
+	  	console.log(controller.playerOptions)
+	    expect(controller.view.renderMapPlayerMarkers).toHaveBeenCalledWith(controller.playerOptions);
+	  });
+	})
 });
 
 
