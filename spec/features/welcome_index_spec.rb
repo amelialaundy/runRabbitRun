5d58507814423a3fb44e6cf9abcd69c2ecc1753f
Amelia Laundy <amelialaundy@gmail.com>
2014-09-29 11:57:15 +1300

    features test on welcome page passing

--- spec/features/welcome_index_spec.rb
+++ spec/features/welcome_index_spec.rb
@@ -13,15 +13,3 @@ describe "the start new game process", :type => :feature do
   end
 end
 
-
-
-# describe "the search location process", :type => :feature do
-#   it "searches for a location" do
-#     visit '/'
-#     within("#search") do
-#       fill_in 'address', :with => '15 walter street te aro'
-#     end
-#     click_button 'search'
-#     expect(page).to have_content 'Walter'
-#   end
-# end
