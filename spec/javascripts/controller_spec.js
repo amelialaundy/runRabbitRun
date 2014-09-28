3bdf8d2fd3b3992b984b103623c6a97381d538b9
Amelia Laundy <amelialaundy@gmail.com>
2014-09-28 16:16:48 +1300

    controller testing of bind events, send player position and check winstate done

--- spec/javascripts/controller_spec.js
+++ spec/javascripts/controller_spec.js
@@ -1,23 +1,10 @@
 describe("PlayerController", function() {
-
+	var controller = null;
 	describe("attributes on creation", function() {
-		// beforeEach(function() {
-		//     var controller = new PlayerController();
-		//   });
-	  // it("creates a new View", function() {
-	  // 	controller = new PlayerController();
-	  // 	allow(DomManager.getlATlng).to 
-	  //   expect(controller.view).toBeDefined();
-	  // });
-
-	})
-
-
-
-
-	describe("attributes on creation", function() {
+		var myBindEvents = null;
 	  beforeEach(function() {
 	  	var myView = spyOn(window, 'View');
+	  	var myBindEvents = spyOn(document, 'addEventListener');
 	  	controller = new PlayerController();
 	  });
 
@@ -29,8 +16,85 @@ describe("PlayerController", function() {
 	    expect(controller.playerOptions).toBeDefined();
 	  });
 
-	  // it("stops all execution on a function", function() {
-	  //   expect(bar).toBeNull();
-	  // });
+	  it("creates attributes of null for biggest lat", function() {
+	    expect(controller.biggestLat).toBeNull();
+	  });
+
+	  it("creates attributes of null for biggest lng", function() {
+	    expect(controller.biggestLng).toBeNull();
+	  });
+
+	  it("creates attributes of null for smallest lat", function() {
+	    expect(controller.smallestLat).toBeNull();
+	  });
+
+	  it("creates attributes of null for smallest lng", function() {
+	    expect(controller.smallestLng).toBeNull();
+	  });
+
+	  it("has a start function", function() {
+	    expect(controller.start).toBeDefined();
+	  });
+
+	  it("bind events adds a keyup event listener to the document", function() {
+	  	controller.bindEvents();
+	  	this.bindEventsArgs = document.addEventListener.calls.argsFor(0);
+	    expect(this.bindEventsArgs[0]).toEqual('keyup');
+	  });
+
+	  it("bind events sets the keyup call back to the sendPlayerPosition function", function() {
+	  	controller.bindEvents();
+	  	this.bindEventsArgs = document.addEventListener.calls.argsFor(0);
+	    expect(this.bindEventsArgs[1]).toEqual(jasmine.any(Function));
+	  });
 	})
+
+	describe("sending player position", function() {
+		beforeEach(function() {
+			// spyOn(controller, 'checkWinState');
+
+			spyOn($, 'ajax');
+			controller.playerOptions = {
+			    lat: -41.295308,
+			    lng: 174.773082,
+			    id: 1,
+			    game_id: 1,
+			    kind: "rabbit"
+			};
+			controller.sendPlayerPosition();
+
+			this.requestArgs = $.ajax.calls.argsFor(0);
+		})
+
+		it("makes a POST request", function () {
+	    expect(this.requestArgs[0].type).toEqual('POST');
+	  });
+
+  	it("makes a POST request to '/player/update_position' ", function () {
+      expect(this.requestArgs[0].data).toEqual(controller.playerOptions);
+    });
+
+	})
+
+		describe("check win state", function() {
+			beforeEach(function() {
+				spyOn(window, 'clearInterval');
+				spyOn(window, 'alert')
+
+				// spyOn(PlayerController, 'locationTimer');
+				this.data = {
+				    game_status: true
+				};
+				controller.checkWinState(this.data);
+			})
+
+			it("stops the timer when the game is won", function () {
+		    expect(clearInterval).toHaveBeenCalled();
+		  });
+
+		})
 });
+
+
+
+
