ad366b578bc0028c42e38efe73ada1cd2d4bbbe0
Adam Ron <apprentice@Fiji.local>
2014-09-29 11:13:20 +1300

    added welcome controller tests, passed

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe WelcomeController, :type => :controller do
+  RSpec.describe WelcomeController, :type => :controller do
+
+  describe "#index" do
+
+    it "Acts as the home page" do
+      expect(:get => "/").to route_to(
+        :controller => 'welcome',
+        :action => 'index')
+    end
+
+    it "assigns @games" do
+      game = Game.create( active:true)
+      get :index
+      expect(assigns(:games)).to eq([game])
+    end
+
+  end
 
 end
