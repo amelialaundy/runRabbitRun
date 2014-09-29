64f0222df606f15ec48a180e20172e8f052bf139
Amelia Laundy <amelialaundy@gmail.com>
2014-09-30 10:04:56 +1300

    fixing error of extra end

--- spec/controllers/rabbit_controller_spec.rb
+++ spec/controllers/rabbit_controller_spec.rb
@@ -2,10 +2,5 @@ require 'rails_helper'
 
 RSpec.describe RabbitController, :type => :controller do
 
-  # describe "rabbit update" do
-  #   it "sends back valid data" do
-  #     post '/rabbit/update_rabbit_street_view'
-  #   end
-
-  end
+ 
 end
