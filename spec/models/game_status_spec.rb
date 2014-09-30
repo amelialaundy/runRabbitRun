012755e640e21e878654002f6380e5078e1634e8
stevenmathews <stevenmathews.nz@gmail.com>
2014-09-30 18:57:06 +1300

    more tests written for #update

--- spec/models/game_status_spec.rb
+++ spec/models/game_status_spec.rb
@@ -4,9 +4,13 @@ RSpec.describe GameStatus, :type => :model do
 
   describe "#update" do
 
-    let(:game) { instance_double("Game") }
+    let(:game) { instance_double("Game", finished!: false) }
     let(:game_status) { GameStatus.new(game) }
     let(:player) { instance_double("Player", rabbit?: true) }
+    let(:hunter) { create(:player, kind: "hunter")}
+    # let(:hunter) { instance_double("Player", kind: "hunter") }
+    let(:rabbit) { instance_double("Player", kind: "rabbit") }
+    let(:proximity) { ProximityChecker.new({hunter: hunter, rabbit: rabbit})}
 
     before do
       allow(Player).to receive(:find).and_return(player)
@@ -23,11 +27,21 @@ RSpec.describe GameStatus, :type => :model do
       expect(result).to eq({ proximity: "irrelevent"})
     end
 
-    it "returns 'win_zone' if the hunter is very close to the rabbit" do
+    it "ends the game if the hunter is very close to the rabbit" do
       allow(player).to receive(:update_position)
-      result = game_status.update({id: 1, lat: 3, lng: 5})
-      expect(result).to eq({proximity: "win_state"})
+      allow(ProximityChecker).to receive(:new).and_return(proximity)
+      allow(proximity).to receive(:win_zone?).and_return(true)
+      game_status.update({id: 1, lat: 3, lng: 5})
+      expect(game).to receive(:finished!).and_return(false)
     end
+
+    # it "returns 'win_zone' if the hunter is very close to the rabbit" do
+    #   allow(player).to receive(:update_position)
+    #   result = game_status.update({id: 1, lat: 3, lng: 5})
+    #   expect(result).to eq({proximity: "win_zone"})
+    # end
+
+
 
 
 
