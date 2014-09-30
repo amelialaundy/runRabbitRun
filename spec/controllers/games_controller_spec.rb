f6598a1f1f8bd9b3b46f3f78f8103152b73a23c0
Adam Ron <apprentice@Fiji.local>
2014-10-01 11:42:17 +1300

    finished refactoring gameController and tests

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -5,37 +5,33 @@ RSpec.describe GamesController, :type => :controller do
   describe "#create" do
 
     let(:send_params) do
-        { data: { results: { "0" => { geometry: { location: {
-            lat: "-41.29526",
-            lng: "174.77248"
-        }}}}}}
+      { data: { results: { "0" => { geometry: { location: {
+         lat: "-41.29526",
+          lng: "174.77248"
+      }}}}}}
     end
     let(:params) {{centre_lat: "-41.29526", centre_lng: "174.77248", active:true}}
 
-  # before do
-  #   allow(GamesController).to receive(:get_params).and_return(send_params)
-  # end
-
-  it "receives json parameters" do
-    game = double(:game, id:4)
-    expect(Game).to receive(:create).with(params).and_return(game)
-    post :create, send_params
-  end
-
-  context "hits the database", db:true do
-    it "creates a game" do
-      expect { post :create, send_params }.to change {Game.count}.by(1)
+    it "receives json parameters" do
+      game = double(:game, id:4)
+      expect(Game).to receive(:create).with(params).and_return(game)
+      post :create, send_params
     end
-  end
+
+    context "hits the database", db:true do
+      it "creates a game" do
+        expect { post :create, send_params }.to change {Game.count}.by(1)
+      end
+    end
 
     it "gives the game the right params" do
-    	post :create, params
+      post :create, send_params
 
-    	game = Game.last
-    	expect(game.centre_lat.to_s).to eq params[:centre_lat]
-    	expect(game.centre_lng.to_s).to eq params[:centre_lng]
+      game = Game.last
+      expect(game.centre_lat.to_s).to eq params[:centre_lat]
+      expect(game.centre_lng.to_s).to eq params[:centre_lng]
     end
-	end
+  end
 
 	describe "#show" do
 
