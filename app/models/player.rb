93cf8bc5381681de01aede8dfda86ba1a5299a5a
stevenmathews <stevenmathews.nz@gmail.com>
2014-09-30 14:26:35 +1300

    added #update_position and #rabbit? to Rabbit with tests

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,8 +1,16 @@
 class Player < ActiveRecord::Base
   belongs_to :game
 
-  def hunter?
-  	self.kind == "hunter"
+  RABBIT = 'rabbit'
+
+  def update_position(position_hash)
+    self.lat = position_hash[:lat]
+    self.lng = position_hash[:lng]
+    save
+  end
+
+	def rabbit?
+  	self.kind == RABBIT
   end
 
 end
