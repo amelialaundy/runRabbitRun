db603e8cde15243e1b5e4d26bacf40248ab24e85
Amelia Laundy <amelialaundy@gmail.com>
2014-09-30 17:10:34 +1300

    adding limits for three levels of proximity

--- app/models/proximity_checker.rb
+++ app/models/proximity_checker.rb
@@ -1,10 +1,13 @@
 class ProximityChecker
 
 	attr_reader :hunter, :rabbit
-
+	WIN_ZONE_RADIUS = 0.0005
+	RED_ZONE_RADIUS = 0.002
+	YELLOW_RADIUS = 0.004
 	def initialize(players)
 		@hunter = players[:hunter]
 		@rabbit = players[:rabbit]
 	end
+
 
 end
