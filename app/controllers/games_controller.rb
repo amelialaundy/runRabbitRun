6c5dcd0f374698ff046c899ffaf930e4ce2faa98
Adam Ron <apprentice@Fiji.local>
2014-10-01 09:52:08 +1300

    tidied up gamescontroller with smaller private methods

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,25 +5,30 @@ class GamesController < ApplicationController
 
   def create
     game_params = GamesController.get_params(params)
-    game = Game.new(centre_lat:game_params[:lat],
+    game = Game.create(centre_lat:game_params[:lat],
                      centre_lng:game_params[:lng],
                      active: true)
-    game.save
     render json: {game_id: game.id}
   end
 
   def show
     @game = Game.find(params[:id])
-    @player = Player.create( game_id: params[:id],
-                          lat: @game.get_random_lat,
-                          lng: @game.get_random_lng,
-                          kind: @game.mark_as_rabbit? )
+    @player = Player.create(game_details(@game))
   end
 
   def self.get_params(params)
     return {
       lat: params['data']['results']['0']['geometry']['location']['lat'],
       lng: params['data']['results']['0']['geometry']['location']['lng']
+    }
+  end
+
+private
+  def get_game_details(game)
+    { game_id: @game.id,
+      lat: @game.get_random_lat,
+      lng: @game.get_random_lng,
+      kind: @game.mark_as_rabbit?
     }
   end
 
