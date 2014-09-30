367f26e40f40b59ef0ff43fad35c99c12627cf50
stevenmathews <stevenmathews.nz@gmail.com>
2014-10-01 10:24:51 +1300

    Player model and tests refactored

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -5,20 +5,21 @@ RSpec.describe Player, :type => :model do
 	describe "#update_position" do
 
 		let(:position) { {lat: 4, lng: 6} }
+		let(:update) { subject.update_position(position) }
 
 		it "updates the player's lat" do
-			subject.update_position(position)
+			update
 			expect(subject.lat).to eq(4)
 		end
 
 		it "updates the player's lng" do
-			subject.update_position(position)
+			update
 			expect(subject.lng).to eq(6)
 		end
 
 		it "saves to the database" do
 			expect(subject).to receive(:save)
-			subject.update_position(position)
+			update
 		end
 
 	end
@@ -26,12 +27,12 @@ RSpec.describe Player, :type => :model do
 	describe "#rabbit?" do 
 
 		it "returns false if the player is not a rabbit" do
-			player = Player.new(kind: "hunter")
+			player = build(:player, kind: "hunter")
 			expect(player.rabbit?).to eq(false)
 		end
 
 		it "returns true if the player is a rabbit" do
-			player = Player.new(kind: "rabbit")
+			player = build(:player, kind: "rabbit")
 			expect(player.rabbit?).to eq(true)
 		end
 
