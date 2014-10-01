1ee093ccc7ce684fe1434fa1b28c6350de372b52
Steve <stevenmathews.nz@gmail.com>
2014-10-01 15:30:15 +1300

    Update rabbit_controller.rb

--- app/controllers/rabbit_controller.rb
+++ app/controllers/rabbit_controller.rb
@@ -1,7 +1,6 @@
 class RabbitController < ApplicationController
 
   def update_rabbit_street_view
-  	p "this is the rabbit params:#{params['game_id']}"
     latlng = params["lat"]+','+params["lng"]
     Pusher['game_'+params["game_id"].to_s].trigger('show_rabbit_street_view_game', {
       :message => latlng
