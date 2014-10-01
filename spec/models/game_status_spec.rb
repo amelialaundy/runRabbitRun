6cfe526934da7f79fbadec2997b4a2a9d5ad1844
stevenmathews <stevenmathews.nz@gmail.com>
2014-10-01 21:11:18 +1300

    game status spec refactored, proximity tests created

--- spec/models/game_status_spec.rb
+++ spec/models/game_status_spec.rb
@@ -4,42 +4,55 @@ RSpec.describe GameStatus, :type => :model do
 
   describe "#update" do
 
-    let(:game) { instance_double("Game", finished!: false) }
+    let(:game) { instance_double("Game") }
     let(:game_status) { GameStatus.new(game) }
-    let(:player) { instance_double("Player", rabbit?: true) }
-    let(:hunter) { create(:player, kind: "hunter")}
-    # let(:hunter) { instance_double("Player", kind: "hunter") }
-    let(:rabbit) { instance_double("Player", kind: "rabbit") }
-    let(:proximity) { ProximityChecker.new({hunter: hunter, rabbit: rabbit})}
+    let(:player) { instance_double("Player") }
+    let(:game_status_update) { game_status.update({id: 1, lat: 3, lng: 5}) }
 
     before do
       allow(Player).to receive(:find).and_return(player)
-    end
-
-    it "updates the player's location" do
-      expect(player).to receive(:update_position).with(lat: 3, lng: 5)
-      game_status.update({id: 1, lat: 3, lng: 5})
-    end
-
-    it "returns 'irrelevent' if the player is a rabbit" do
       allow(player).to receive(:update_position)
-      result = game_status.update({id: 1, lat: 3, lng: 5})
-      expect(result).to eq({ proximity: "irrelevent"})
+    end 
+
+    context "player is a rabbit" do
+
+      before do
+        allow(player).to receive(:rabbit?).and_return(true)
+      end
+
+      it "updates the player's location (for rabbit and hunter)" do
+        expect(player).to receive(:update_position).with(lat: 3, lng: 5)
+        game_status_update
+      end
+
+      it "returns 'irrelevent' if the player is a rabbit" do
+        expect(game_status_update).to eq({ proximity: "irrelevent"})
+      end
+
     end
 
-    # it "ends the game if the hunter is very close to the rabbit" do
-    #   allow(player).to receive(:update_position)
-    #   allow(ProximityChecker).to receive(:new).and_return(proximity)
-    #   allow(proximity).to receive(:win_zone?).and_return(true)
-    #   game_status.update({id: 1, lat: 3, lng: 5})
-    #   expect(game).to receive(:finished!).and_return(false)
-    # end
+    context "player is a hunter" do
 
-    # it "returns 'win_zone' if the hunter is very close to the rabbit" do
-    #   allow(player).to receive(:update_position)
-    #   result = game_status.update({id: 1, lat: 3, lng: 5})
-    #   expect(result).to eq({proximity: "win_zone"})
-    # end
+      let(:proximity) { instance_double("Proximity", win?: true) } 
+
+      before do
+        allow(player).to receive(:rabbit?).and_return(false)
+        allow(game).to receive(:find_rabbit)
+        allow(ProximityChecker).to receive(:new).and_return(proximity)
+        allow(proximity).to receive(:zone).and_return("win")
+      end
+
+      it "ends the game if the hunter is very close to the rabbit" do
+        expect(game).to receive(:finished!)
+        game_status_update
+      end
+
+      it "returns 'win_zone' if the hunter is very close to the rabbit" do
+        allow(game).to receive(:finished!)
+        expect(game_status_update).to eq({proximity: "win"})
+      end
+
+    end
 
   end
 
