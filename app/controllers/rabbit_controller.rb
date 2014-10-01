baca0094dcb1c1c22d99d17bb49efb5a48ad112b
Adam Ron <apprentice@Fiji.local>
2014-10-01 15:37:56 +1300

    Adding tests to rabbit controller

--- app/controllers/rabbit_controller.rb
+++ app/controllers/rabbit_controller.rb
@@ -1,12 +1,15 @@
 class RabbitController < ApplicationController
 
   def update_rabbit_street_view
-    latlng = params["lat"]+','+params["lng"]
+    latlng = concat_coords(params["lat"],params["lng"])
     pusher_rabbit_location(latlng)
     render json: {latlng: latlng}
   end
 
 private
+  def concat_coords(lat,lng)
+    params["lat"]+','+params["lng"]
+  end
 
   def pusher_rabbit_location(latlng)
     Pusher['game_'+params["game_id"].to_s].trigger('show_rabbit_street_view_game', {
--- spec/controllers/rabbit_controller_spec.rb
+++ spec/controllers/rabbit_controller_spec.rb
@@ -2,5 +2,17 @@ require 'rails_helper'
 
 RSpec.describe RabbitController, :type => :controller do
 
- 
+  describe "#update rabbit street view" do
+    let(:params) {{
+        "lat"=>"-41.29526",
+        "lng"=>"174.77248"
+      }}
+
+    xit "receives an ajax POST call" do
+      expect(latlng).to receive(params["lat"],params["lng"])
+      post :update_rabbit_street_view, params
+    end
+
+  end
+
 end
