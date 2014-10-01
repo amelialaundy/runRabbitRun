8f27acee541f38c41ab1aaca4f7aa14cb15c7677
Adam Ron <apprentice@Fiji.local>
2014-10-01 15:06:28 +1300

    Added some controller tests, some difficulty with playerController_spec.rb

--- spec/controllers/players_controller_spec.rb
+++ spec/controllers/players_controller_spec.rb
@@ -1,19 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe PlayersController, :type => :controller do
+	let(:params) {{
+								"id"=>"1",
+								"lat"=>"-41.29526",
+								"lng"=>"174.77248",
+								"game_id"=>"1",
+								"controller"=>"players",
+								"action"=>"update_player_position"
+							}}
+	before do
+		test_game = create(:game)
+	end
 
 	describe '#update_player_position' do
-		let(:params) {{
-										"id"=>"4",
-										"lat"=>"-41.29526",
-										"lng"=>"174.77248",
-										"game_id"=>"1",
-										"controller"=>"players",
-										"action"=>"update_player_position"
-									}}
 
 		it "#update receives json from ajax" do
-			expect(GameStatus).to receive(:update).with(params)
+			expect(GameStatus).to receive(:new).with(:test_game)
       post :update_player_position, params
 		end
 
@@ -23,6 +26,17 @@ RSpec.describe PlayersController, :type => :controller do
 			expect(response.body).to eq("{\"proximity\":true}")
 		end
 
+	end
+
+	describe "#send_win_message" do
+
+		xit "receives data from ajax" do
+			push_db = double(:Pusher, trigger:4)
+			post :send_win_message, params
+		end
+
+		xit "returns a message" do
+		end
 	end
 
 end
